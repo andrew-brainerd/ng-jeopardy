aefb87b1fb3ce37fcda788cafb0dfde1b5e07f68
Andrew Brainerd <drwb333@gmail.com>
2020-10-14 15:15:40 -0400

    Reorganize project

--- services/giphy.service.js
+++ services/giphy.service.js
@@ -1,5 +1,3 @@
-var module = angular.module('app');
-
 var giphy = function($http, __env) {
   var getRandomGif = function(tag) {
     return $http.get('https://api.giphy.com/v1/gifs/random?api_key=' + __env.GIPHY_API_KEY + '&tag=' + tag);
@@ -10,4 +8,6 @@ var giphy = function($http, __env) {
   };
 };
 
-module.factory('giphy', ['$http', '__env', giphy]);
+angular
+  .module('app')
+  .factory('giphy', ['$http', '__env', giphy]);
